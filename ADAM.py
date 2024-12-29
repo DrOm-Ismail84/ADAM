60b46dd93d838c70ab6965163cec960dd931f3ba
DrOm-Ismail84 <142762308+DrOm-Ismail84@users.noreply.github.com>
2024-12-29 11:55:52 +0800

    Add files via upload

--- ADAM.py
+++ ADAM.py
@@ -59,9 +59,9 @@ if L < m.sqrt(20*D*t):
 elif L > m.sqrt(20*D*t):
     P_ASME_B31G = (2*t*UTS/D)*(1-(Dc/t))
 
-# Calculate burst pressure of corrorded pipe P LPC Model
-Q = m.sqrt(1+0.31*(L/(m.sqrt(D*t)))**2) #Q is the curved fit of FEA results
-P_LPC = 2*t*UTS/D-t*(1-Dc/t/1-Dc/Q*t)
+# Calculate burst pressure of corrorded pipe PDnV
+Q = m.sqrt(1+0.31*(Lc)**2/D*t) #Q is the curved fit of FEA results
+P_DnV = 2*t*UTS*(1-Dc/t)/D-t*(1-Dc/t/Q)
 
 # Calculate burst pressure of corroded pipe P PCORRC Model 
 P_PCORRC = (2*t*UTS/D)*(1-Dc/t)
