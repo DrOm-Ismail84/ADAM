0e3c54a4e8e1d0ad5c176010cd930eeb91993123
DrOm-Ismail84 <142762308+DrOm-Ismail84@users.noreply.github.com>
2024-12-30 11:08:23 +0800

    ADAM.py

--- ADAM.py
+++ ADAM.py
@@ -106,15 +106,17 @@ st.subheader('Calculated Corrorded Pipe Burst Pressure via PCORRC')
 st.write(calculated_param_df)
 
 #Data
-df = pd.DataFrame (
+data = {
     "Pvm (MPa)": Pvm,
     "PTresca (MPa)": PTresca,
     "P_ASME_B31G (MPa)": P_ASME_B31G,
     "P_DnV (MPa)": P_DnV,
     "P_PCORRC (MPa)": P_PCORRC,
-)
+}
 
-st.bar_chart(pd.DataFrame, x="Design Codes", y="Burst Pressure (MPa)", color="site", horizontal=True)
+df = pd.DataFrame(data)
+
+st.bar_chart(data, x="Design Codes", y="Burst Pressure (MPa)", color="site", horizontal=True)
 
 st.subheader('Reference')
 st.write('Xian-Kui Zhu, A comparative study of burst failure models for assessing remaining strength of corroded pipelines, Journal of Pipeline Science and Engineering 1 (2021) 36 - 50, https://doi.org/10.1016/j.jpse.2021.01.008')
