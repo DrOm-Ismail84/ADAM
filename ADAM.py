39ff2666ac2f99f9c7301f0737dad75edccfd252
DrOm-Ismail84 <142762308+DrOm-Ismail84@users.noreply.github.com>
2025-01-16 09:11:05 +0800

    ADAM.py

--- ADAM.py
+++ ADAM.py
@@ -24,7 +24,7 @@ def user_input_features():
     corrosion_length = st.sidebar.number_input('Corrosion Length, Lc (mm)', value = 0.01)
     corrosion_depth = st.sidebar.number_input('Corrosion Depth, Dc (mm)', value = 0.01)
     UTS = st.sidebar.number_input('Ultimate Tensile Strength, UTS (MPa)', value = 0.01)
-    Maximum_Operating_Pressure = st.sidebar.slider('Maximum Operating Pressure, Pop, Max (MPa)', min_value=10, max_value=50, step=20)
+    Maximum_Operating_Pressure = st.sidebar.slider('Maximum Operating Pressure, Pop, Max (MPa)', min_value=10, max_value=50, step=5)
     Minimum_Operating_Pressure = st.sidebar.slider('Minimum Operating Pressure, Pop, Min (MPa)', min_value=1, max_value=5, step=5)
 
     data = {'t (mm)': pipe_thickness,
